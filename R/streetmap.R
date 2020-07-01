a5f510515c04a4b3ae2264d1ebecd3c8986f83c6
Rob <rharbert@stonehill.edu>
2020-07-01 15:08:40 -0400

    update streetmap.R

--- R/streetmap.R
+++ R/streetmap.R
@@ -5,11 +5,23 @@ library(ggplot2)
 library(rgdal)
 library(sf)
 library(landscapetools)
+library(knor)
+library(RStoolbox)
 
+# bounding latitude and longitude of your study area (think small, or expect long wait times)
+# You may pick these by browsing google maps and right clicking on the map and selecing "What's here?"
+# this will display a lat/long set
+#
+# Pick minimum and maximum lat and long and enter as:
+# lon_min, lon_max, lat_min, lat_max
+lon_min = -75.42
+lon_max = -75.27
+lat_min = 37.83544
+lat_max = 37.97
+ext = extent(c(lon_min, lon_max, lat_min, lat_max)) # EDIT if you are searching for another place
 
-ext = extent(c(-75.42, -75.27, 37.83544, 37.97))
-
-place = 'Chincoteague Virginia'
+# Get Open Street Map (OSM) Data
+place = 'Chincoteague Virginia' # EDIT if you are searching for another place
 
 streets <- getbb(place) %>%
   opq() %>%
@@ -21,8 +33,8 @@ streets
 streets <- getbb(place)%>%
   opq()%>%
   add_osm_feature(key = "highway", 
-                  value = c("motorway", "primary", 
-                            "secondary", "tertiary")) %>%
+                  value = c( "primary", 
+                             "secondary", "tertiary")) %>%
   osmdata_sf()
 streets
 
@@ -30,37 +42,59 @@ small_streets <- getbb(place)%>%
   opq()%>%
   add_osm_feature(key = "highway", 
                   value = c("residential", "living_street",
-                            "unclassified",
                             "service")) %>%
   osmdata_sf()
 
-# streets.w <- getbb("Wallops Island Virginia") %>%
-#   opq() %>%
-#   add_osm_feature(key = "highway",
-#                   value = c(available_tags("highway"))) %>%
-#   osmdata_sf()
-# streets.w
-
-
+######## END OSM ################
 
 # # Get Landsat8 images for bands 1-4 (or 3-4 for NDVI)
-# get1= 'wget https://s3-us-west-2.amazonaws.com/landsat-pds/c1/L8/014/034/LC08_L1TP_014034_20191024_20191030_01_T1/LC08_L1TP_014034_20191024_20191030_01_T1_B1.TIF'
-# get2= 'wget https://s3-us-west-2.amazonaws.com/landsat-pds/c1/L8/014/034/LC08_L1TP_014034_20191024_20191030_01_T1/LC08_L1TP_014034_20191024_20191030_01_T1_B2.TIF'
-# get3= 'wget https://s3-us-west-2.amazonaws.com/landsat-pds/c1/L8/014/034/LC08_L1TP_014034_20191024_20191030_01_T1/LC08_L1TP_014034_20191024_20191030_01_T1_B3.TIF'
-# get4= 'wget https://s3-us-west-2.amazonaws.com/landsat-pds/c1/L8/014/034/LC08_L1TP_014034_20191024_20191030_01_T1/LC08_L1TP_014034_20191024_20191030_01_T1_B4.TIF'
-get5= 'wget https://s3-us-west-2.amazonaws.com/landsat-pds/c1/L8/014/034/LC08_L1TP_014034_20191024_20191030_01_T1/LC08_L1TP_014034_20191024_20191030_01_T1_B5.TIF'
 
-# 
-# 
-# system(get1)
-# system(get2)
-# system(get3)
-# system(get4)
+# modify for another location
+# learn more about these parameters here:https://www.earthdatascience.org/courses/earth-analytics/multispectral-remote-sensing-data/landsat-data-in-r-geotiff/
+# and also here:https://docs.opendata.aws/landsat-pds/readme.html 
+
+download.file('https://landsat-pds.s3.amazonaws.com/c1/L8/scene_list.gz', 'data/scene_list.gz')
+system('gunzip data/scene_list.gz')
+
+scene_list = data.table::fread('data/scene_list', 
+                               sep = ',', 
+                               colClasses = c('character',
+                                              'character',
+                                              'character',
+                                              'numeric',
+                                              'character',
+                                              'character', 
+                                              'character',
+                                              'numeric',
+                                              'numeric',
+                                              'numeric',
+                                              'numeric',
+                                              'character'))
+
+aoi= scene_list %>% filter(min_lon <= lon_min &
+                             min_lat <= lat_min &
+                             max_lon >= lon_max &
+                             max_lat >= lat_max) %>% # filter for paths and rows covering study area
+  filter(cloudCover < 5) %>% # Get cloud cover < 10%
+  filter(acquisitionDate == max(acquisitionDate))  %>% # get most recent image
+  slice(1) %>% # keep only one row
+  mutate(path = str_pad(path, 3, pad='0')) %>%
+  mutate(row = str_pad(row, 3, pad='0'))
+
+download_base = paste('https://s3-us-west-2.amazonaws.com/landsat-pds/c1/L8/', aoi$path, "/", aoi$row, "/", aoi$productId, "/", aoi$productId, sep='')
+
+get4 = paste( 'wget ', download_base, '_B4.TIF', sep = '')
+get5 = paste( 'wget ', download_base, '_B5.TIF', sep = '')
+
+system(get4)
 system(get5)
 
 l8 = raster::stack(list.files(pattern='*TIF', full.names = TRUE))
 
+### END get LANDSAT ###
 
+
+# begin processing
 crs.streets = crs(streets$osm_lines)
 
 l8.repro <- projectRaster(l8, crs=crs.streets)
@@ -69,135 +103,90 @@ ls.ex = crop(l8.repro, ext)
 l8_df = raster::as.data.frame(ls.ex, xy = TRUE)
 head(l8_df)
 
-NDVI = (ls.ex[[5]] - ls.ex[[4]])/(ls.ex[[5]] + ls.ex[[4]])
-ndvi_df = as.data.frame(NDVI, xy=T)
-## ggplot 
+NDVI = (ls.ex[[2]] - ls.ex[[1]])/(ls.ex[[2]] + ls.ex[[1]])
 
-ggplot() +
-  geom_raster(data = ndvi_df, 
-              aes(x = x, 
-                  y = y, 
-                  fill = layer
-                  ), interpolate=T
-              ) +
-  scale_fill_gradientn(
-    colours=c('white', 'darkred'),
-    na.value = "black"
-    )+
-  geom_sf(
-    data = streets$osm_lines,
-    inherit.aes = FALSE,
-    color = "black",
-    size = .4,
-    alpha = .8
-  ) + 
-  geom_sf(
-    data = small_streets$osm_lines,
-    inherit.aes = FALSE,
-    color = "goldenrod",
-    size = .4,
-    alpha = .8
-  ) + 
-  coord_sf(
-    xlim = c(ext[1], ext[2]),
-    ylim = c(ext[3], ext[4]),
-    expand = FALSE
-  ) + 
-  theme_void()
-
-
-
-
-# Classify landsat raster
-n=3
-classified_landscape <- util_classify(NDVI,
-                                      n = n
-                                      )
-
-
-classif_disag = disaggregate(classified_landscape, fact=c(4,4))
-classif_res = resample(classified_landscape, classif_disag, method='ngb')
 #try focal here:focal(x, w=matrix(1, 5, 5), mean)
 
+#smooth NDVI raster to avoid noise
+NDVI.d = disaggregate(NDVI, fact=c(8,8))
+NDVI.f = focal(NDVI.d, w=matrix(1,7,7), mean)
+NDVI.f = focal(NDVI.f, w=matrix(1,5,5), mean)
+NDVI.f = focal(NDVI.f, w=matrix(1,3,3), mean)
 
+NDVI.uc <- unsuperClass(NDVI.f, nClasses = 3)
 
-classif_df = as.data.frame(classif_res, xy=TRUE)
+ndvi_df = as.data.frame(NDVI.uc$map, xy=T)
 
-#classif_poly = rasterToPolygons(classified_landscape>2, 
-             #                   fun=NULL, n=16, na.rm=TRUE, digits=8, dissolve=T)
+## ggplot 
 
-#cpoly_df = as.data.frame(classif_poly2, xy=TRUE)
-
-#sf_poly <- as(classif_poly, "sf")
-#sf::st_crs(sf_poly) <- 4326
-#sf_poly$id <- c(1,2)
-#(poly_plot = ggplot(sf_poly) +
- # geom_sf(aes(fill = as.factor(id)), lwd=0) +
-  #scale_fill_manual(values=c('black', 'gray95')) +
-  
-(poly_plot = ggplot() +
-    geom_raster(data=classif_df, 
-                aes(x=x, y=y, fill=layer),
-                interpolate=T
-              
-                ) +
+(ndvi.cat = ggplot() +
+    geom_raster(data = ndvi_df, 
+                aes(x = x, 
+                    y = y, 
+                    fill = layer
+                ), interpolate=T, show.legend=F
+    ) +
     scale_fill_gradientn(
-       colours=c('black', 'gray90', 'steelblue'),
-       na.value = "black"
-     ) +
-  geom_sf(
-    data = streets$osm_lines,
-    inherit.aes = FALSE,
-    color = "goldenrod",
-    size = .5,
-    alpha = .8
-  ) + 
+      colours=c('black', 'darkblue', 'steelblue'),
+      na.value = "black"
+    )+
+    geom_sf(
+      data = streets$osm_lines,
+      inherit.aes = FALSE,
+      color = "white",
+      size = .4,
+      alpha = .8
+    ) + 
     geom_sf(
       data = small_streets$osm_lines,
       inherit.aes = FALSE,
-      color = "goldenrod",
-      size = .3,
+      color = "white",
+      size = .4,
       alpha = .8
     ) + 
-  coord_sf(
-    xlim = c(ext[1], ext[2]),
-    ylim = c(ext[3], ext[4]),
-    expand = FALSE
-  ) + theme_void() + 
-  theme(legend.position = "none") 
-)
-ggsave(poly_plot, filename='map_poly.png', height = 9, width = 9, dpi=400, units='in')
+    coord_sf(
+      xlim = c(ext[1], ext[2]),
+      ylim = c(ext[3], ext[4]),
+      expand = FALSE
+    ) + 
+    theme_void()) + 
+  theme(legend.position = "none")
+
+ggsave(ndvi.cat , filename='map_cat.png', width=9, height=9, dpi=900, units='in')
+
+### END ###
 
 
-# RGB realcolor plot
-
-
-#Scale to 0-255 range for display device
-rgb_ras <- stretch(x=ls.ex, minv=0, maxv=255)
-
-rgb_df = raster::as.data.frame(rgb_ras, xy = TRUE)
-
-colnames(rgb_df)= c('x','y','B1','B2','B3','B4','B5')
-colorvec = rgb(rgb_df$B4, rgb_df$B3, rgb_df$B2, maxColorValue = 255)
-
-ggplot() +
-  geom_raster(data = l8_df, 
-              aes(x = x, 
-                  y = y, 
-                  fill = colorvec
-              ), interpolate=T
-  ) + scale_fill_identity() +
-  geom_sf(
-    data = streets$osm_lines,
-    inherit.aes = FALSE,
-    color = "white",
-    size = .4,
-    alpha = .8
-  ) + 
-  coord_sf(
-    xlim = c(ext[1], ext[2]),
-    ylim = c(ext[3], ext[4]),
-    expand = FALSE
-  ) + 
-  theme_void()
-
+### Notes for later:
+# # RGB realcolor plot
+# 
+# 
+# #Scale to 0-255 range for display device
+# rgb_ras <- stretch(x=ls.ex[[2:4]], minv=0, maxv=255)
+# 
+# rgb_df = raster::as.data.frame(rgb_ras, xy = TRUE)
+# 
+# colnames(rgb_df)= c('x','y', 'B2','B3','B4')
+# colorvec = rgb(rgb_df$B4, rgb_df$B3, rgb_df$B2, maxColorValue = 255)
+# 
+# ggplot() +
+#   geom_raster(data = l8_df, 
+#               aes(x = x, 
+#                   y = y, 
+#                   fill = colorvec
+#               ), interpolate=T
+#   ) + scale_fill_identity() +
+#   geom_sf(
+#     data = streets$osm_lines,
+#     inherit.aes = FALSE,
+#     color = "white",
+#     size = .4,
+#     alpha = .8
+#   ) + 
+#   coord_sf(
+#     xlim = c(ext[1], ext[2]),
+#     ylim = c(ext[3], ext[4]),
+#     expand = FALSE
+#   ) + 
+#   theme_void()
+# 
